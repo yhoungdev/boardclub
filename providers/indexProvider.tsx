32dfec26675757bb6645f139b0fabaf75d29b5b4
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-02-17 14:44:19 +0100

    chore: fixed bug issue

--- providers/indexProvider.tsx
+++ providers/indexProvider.tsx
@@ -1,16 +1,34 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import PrivyAuthProvider from "@/providers/privyProvider";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { ISPRODUCTION } from "@/constant";
+import { usePrivy } from "@privy-io/react-auth";
+import { usePathname, useRouter } from "next/navigation";
+
+const AuthCheck = ({ children }: { children: ReactNode }) => {
+  const { ready, authenticated } = usePrivy();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (ready && !authenticated && pathname !== '/') {
+      router.push('/');
+    }
+  }, [ready, authenticated, pathname, router]);
+
+  return <>{children}</>;
+};
+
 const IndexApplicationProvider = ({ children }: { children: ReactNode }) => {
   const manfiestUrl = ISPRODUCTION
     ? "https://boardclub.vercel.app"
     : "https://localhost:3000";
+
   return (
     <PrivyAuthProvider>
       <TonConnectUIProvider manifestUrl={`${manfiestUrl}/manifest.json`}>
-        {children}
+        <AuthCheck>{children}</AuthCheck>
       </TonConnectUIProvider>
     </PrivyAuthProvider>
   );
