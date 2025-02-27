e58afc59f5a7f7ddcfa06b52ddfa16c448ce5940
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-02-27 16:24:29 +0100

    🗂️  checking for environment and changing TON amount

--- lib/payment.ts
+++ lib/payment.ts
@@ -1,10 +1,11 @@
 import { Address, toNano } from "ton-core";
 import { TonConnectUI } from "@tonconnect/ui-react";
+import { ISPRODUCTION } from "@/constant";
 
 export const sendPayment = async (
   tonConnectUI: TonConnectUI,
   receiverAddress: string,
-  amount: string = "0.2",
+  amount: string = ISPRODUCTION ? "1" : "0.2",
 ) => {
   const tx = {
     validUntil: Math.floor(Date.now() / 1000) + 300,
